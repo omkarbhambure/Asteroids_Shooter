ec106a1ba58e602ec72e0fe7072693207784bf68
omkarbhambure <omieblablablu2996@gmail.com>
2024-11-24 03:41:13 -0500

    Ch2:8: Grouping of players

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     cluck = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
     dt = 0
     # Game loop
@@ -14,9 +20,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
         # pygame.Surface.fill(screen, (0, 0, 0))
-        player.update(dt)
+        for ob in updatable:
+            ob.update(dt)
+        #player.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for ob in drawable:
+            ob.draw(screen)
+        #player.draw(screen)
         pygame.display.flip()
         dt = cluck.tick(60)/1000
 
